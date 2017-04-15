9f14d233e37ef22d2ecb4c7149d425590f59418b
J Huang <i@jhuang.me>
2017-04-15 20:35:42 +0800

    feat(week4): add week4 assignment

---
+++ week4/week4.go
@@ -0,0 +1,116 @@
+package week4
+
+import (
+	"bytes"
+	"crypto/aes"
+	"encoding/hex"
+	"log"
+	"net/http"
+	"time"
+)
+
+func safeXORBytes(dst, a, b []byte) int {
+	n := len(a)
+	if len(b) < n {
+		n = len(b)
+	}
+	for i := 0; i < n; i++ {
+		dst[i] = a[i] ^ b[i]
+	}
+	return n
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func generatePKCS7Padding(paddingCount int) []byte {
+	return bytes.Repeat([]byte{byte(paddingCount)}, paddingCount)
+}
+
+// AddPkcs7 pads given byte array using pkcs7 padding schema till it has blockSize length in bytes
+func addPkcs7(data []byte, blockSize int) []byte {
+
+	var paddingCount int
+
+	if paddingCount = blockSize - (len(data) % blockSize); paddingCount == 0 {
+		paddingCount = blockSize
+	}
+
+	return append(data, generatePKCS7Padding(paddingCount)...)
+}
+
+func GenerateCipherTextOnShortAES(plaintext, ciphertext, target string) string {
+	cipherTextBytes, err := hex.DecodeString(ciphertext)
+	check(err)
+	plaintextBytes := []byte(plaintext)
+	targetBytes := []byte(target)
+
+	iv := cipherTextBytes[:aes.BlockSize]
+	ciphertextWithoutIV := cipherTextBytes[aes.BlockSize:]
+	newIv := make([]byte, aes.BlockSize)
+
+	paddedPlaintext := addPkcs7(plaintextBytes, aes.BlockSize)
+	paddedTarget := addPkcs7(targetBytes, aes.BlockSize)
+	diff := make([]byte, len(paddedPlaintext))
+	safeXORBytes(diff, paddedPlaintext, paddedTarget)
+	safeXORBytes(newIv, iv, diff)
+	var newIvBuf []byte = newIv[:]
+	newCipherText := hex.EncodeToString(append(newIvBuf, ciphertextWithoutIV[:]...))
+	return newCipherText
+
+}
+
+func PaddingOracle(victim string) string {
+	victimBytes, err := hex.DecodeString(victim)
+	check(err)
+	blockNumber := len(victimBytes) / aes.BlockSize
+	plaintext := make([]byte, (blockNumber-1)*aes.BlockSize)
+
+	for blockIndex := blockNumber - 2; blockIndex >= 0; blockIndex-- {
+		for byteIndex := aes.BlockSize - 1; byteIndex >= 0; byteIndex-- {
+			paddingCount := aes.BlockSize - byteIndex
+			injectIndex := blockIndex*aes.BlockSize+byteIndex
+			log.Printf("processing paddingCount = %d", paddingCount)
+
+			ch := make(chan byte)
+
+			for guess := 0x00; guess < 0x100; guess++ {
+				guessByte := byte(guess)
+
+				var testBytes []byte = make([]byte, (blockIndex+2)*aes.BlockSize)
+				copy(testBytes, victimBytes)
+				testBytes[injectIndex] = victimBytes[injectIndex] ^ guessByte ^ byte(paddingCount)
+				for index := injectIndex + 1; index < (blockIndex + 1) * aes.BlockSize; index++ {
+					testBytes[index] = victimBytes[index] ^ plaintext[index] ^ byte(paddingCount)
+				}
+
+				go func (erByte []byte, guessByte byte, injectIndex int, paddingCount int, ch chan byte) {
+					er := hex.EncodeToString(erByte)
+				try:
+					resp, err := http.Get("https://crypto-class.appspot.com/po?er=" + er)
+					if err != nil {
+						log.Println(err)
+						time.Sleep(time.Duration(300)*time.Millisecond)
+						goto try
+					}
+					defer resp.Body.Close()
+					// if the StatusCode is 404, the padding is good but the format is malformed
+					// if the StatusCode is 200 and the paddingCount is as specified, it is exactly the orignal ciphertext so we shall include this situation
+					if resp.StatusCode == 404 || (resp.StatusCode == 200 && paddingCount == int(plaintext[len(plaintext)-1])) {
+						log.Println(guessByte, injectIndex, resp.StatusCode)
+						ch <- guessByte
+					}
+
+				}(testBytes, guessByte, injectIndex, paddingCount, ch)
+			}
+
+			plaintext[injectIndex] = <- ch
+			close(ch)
+		}
+	}
+
+	return string(plaintext[:])
+}
---
+++ week4/week4_test.go
@@ -0,0 +1,18 @@
+package week4
+
+import "fmt"
+
+func ExampleGenerateCipherTextOnShortAES() {
+	plaintext := "Pay Bob 100$"
+	target := "Pay Bob 500$"
+	ciphertext := "20814804c1767293b99f1d9cab3bc3e7ac1e37bfb15599e5f40eef805488281d"
+	newCipherText := GenerateCipherTextOnShortAES(plaintext, ciphertext, target)
+	fmt.Println(newCipherText)
+	// Output: 20814804c1767293bd9f1d9cab3bc3e7ac1e37bfb15599e5f40eef805488281d
+}
+
+func ExamplePaddingOracle() {
+	var er string = "f20bdba6ff29eed7b046d1df9fb7000058b1ffb4210a580f748b4ac714c001bd4a61044426fb515dad3f21f18aa577c0bdf302936266926ff37dbf7035d5eeb4"
+	fmt.Println(PaddingOracle(er))
+	// Output: The Magic Words are Squeamish Ossifrage
+}
