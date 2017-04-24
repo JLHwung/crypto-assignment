b2136e76cb7bc98929b2be2fb62869c9c5deecf0
J Huang <i@jhuang.me>
2017-04-24 23:42:04 +0800

    feat(week6): add q2 answer

--- week6/week6.go
+++ week6/week6.go
@@ -5,30 +5,48 @@ import (
 	"errors"
 )
 
+func isSquareNumber(n *big.Int) (bool, *big.Int) {
+	sqrtFloor := new(big.Int).Sqrt(n)
+	squareSqrtFloor := new(big.Int).Mul(sqrtFloor, sqrtFloor)
+	return squareSqrtFloor.Cmp(n) == 0, sqrtFloor
+}
+
 /* FactorCloselyFactorSemiPrime finds p, q such that
 	N = p*q when |p - q| < 2N^{1/4} and p <= q
  */
-
 func FactorCloselyFactorSemiPrime(N *big.Int) (*big.Int, *big.Int, error) {
+	return FactorNearlyFactorSemiPrime(N, 0)
+}
+
+/* FactorCloselyFactorSemiPrime finds p, q such that
+	N = p*q when |p - q| < 2^{magnitude + 1} N^{1/4} and p <= q
+	Notice that when magnitude = 1, it reduces to `FactorCloselyFactorSemiPrime`
+ */
+
+func FactorNearlyFactorSemiPrime(N *big.Int, magnitude uint) (*big.Int, *big.Int, error) {
 	// A = \lceil\sqrt{N}\rceil
 	NSqrt := new(big.Int).Sqrt(N)
-	NSqrt.Add(NSqrt, big.NewInt(1))
+	AvgGuess := new(big.Int).Set(NSqrt)
+	AvgGuessSqaure := new(big.Int).Mul(AvgGuess, AvgGuess)
+	Difference := new(big.Int).Sub(AvgGuessSqaure, N)
+	one := big.NewInt(1)
 
-	// \lfloor x\rfloor = \sqrt{A^2 - N}\rfloor
-	Difference := new(big.Int).Mul(NSqrt, NSqrt)
-	Difference.Sub(Difference, N)
+	for i := 0; i < (1 << ( magnitude + magnitude)); i++ {
+		// increment Difference by 2*A_guess + 1
+		Difference.Add(Difference, AvgGuess)
+		Difference.Add(Difference, AvgGuess)
+		Difference.Add(Difference, one)
 
-	SqrtDifference := new(big.Int).Sqrt(Difference)
+		// increment A_guess by 1
+		AvgGuess.Add(AvgGuess, one)
 
-	SquaredSqrtDifference := new(big.Int).Mul(SqrtDifference, SqrtDifference);
-
-	// check if x == \lfloor x \rfloor
-	if SquaredSqrtDifference.Cmp(Difference) == 0 {
-		p := new(big.Int).Sub(NSqrt, SqrtDifference)
-		q := new(big.Int).Add(NSqrt, SqrtDifference)
-		return p, q, nil
+		// check if x^2 is square number
+		if isSquare, SqrtDifference := isSquareNumber(Difference); isSquare {
+			p := new(big.Int).Sub(AvgGuess, SqrtDifference)
+			q := new(big.Int).Add(AvgGuess, SqrtDifference)
+			return p, q, nil
+		}
 	}
 
 	return nil, nil, errors.New("The factor is not closely enough for efficient factoring")
 }
-
