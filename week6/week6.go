82aba76104939c4a0fa14fc14062e151f024c0c9
J Huang <i@jhuang.me>
2017-04-25 13:45:03 +0800

    refactor(week6): refactor q2 implementation

--- week6/week6.go
+++ week6/week6.go
@@ -24,31 +24,7 @@ func FactorCloselyFactorSemiPrime(N *big.Int) (*big.Int, *big.Int, error) {
  */
 
 func FactorNearlyFactorSemiPrime(N *big.Int, magnitude uint) (*big.Int, *big.Int, error) {
-	// A = \lceil\sqrt{N}\rceil
-	NSqrt := new(big.Int).Sqrt(N)
-	AvgGuess := new(big.Int).Set(NSqrt)
-	AvgGuessSqaure := new(big.Int).Mul(AvgGuess, AvgGuess)
-	Difference := new(big.Int).Sub(AvgGuessSqaure, N)
-	one := big.NewInt(1)
-
-	for i := 0; i < (1 << ( magnitude + magnitude)); i++ {
-		// increment Difference by 2*A_guess + 1
-		Difference.Add(Difference, AvgGuess)
-		Difference.Add(Difference, AvgGuess)
-		Difference.Add(Difference, one)
-
-		// increment A_guess by 1
-		AvgGuess.Add(AvgGuess, one)
-
-		// check if x^2 is square number
-		if isSquare, SqrtDifference := isSquareNumber(Difference); isSquare {
-			p := new(big.Int).Sub(AvgGuess, SqrtDifference)
-			q := new(big.Int).Add(AvgGuess, SqrtDifference)
-			return p, q, nil
-		}
-	}
-
-	return nil, nil, errors.New("The factor is not closely enough for efficient factoring")
+	return FactorProportionalFactorSemiPrime(N, magnitude, big.NewRat(1, 1))
 }
 
 /* FactorProportionalFactorSemiPrime finds p, q such that
